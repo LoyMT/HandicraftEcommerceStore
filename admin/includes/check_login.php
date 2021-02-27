c74df4e4e785d9f5664d87f820228d7bc9152603
LoyMT <69298245+LoyMT@users.noreply.github.com>
2021-02-27 23:41:10 +0800

    Add files via upload

---
+++ admin/includes/check_login.php
@@ -0,0 +1,9 @@
+<?php
+
+if(!isset($_SESSION['admin_name'])){
+        
+        echo "<script>window.open('../checkout.php','_self')</script>";
+        
+}
+    
+?>
---
+++ admin/includes/sidebar.php
@@ -0,0 +1,112 @@
+<?php 
+    
+    include("check_login.php");
+
+?> 
+   
+<nav class="navbar navbar-inverse navbar-fixed-top"  style="background-color: #FFCC00;)"><!-- navbar navbar-inverse navbar-fixed-top begin -->
+<div class="navbar-header"><!-- navbar-header begin -->
+        
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse"><!-- navbar-toggle begin -->
+            
+            <span class="sr-only">Toggle Navigation</span>
+            
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            
+        </button><!-- navbar-toggle finish -->
+        
+        <a href="index.php?dashboard" class="navbar-brand" style="color: black; text-align:center">Admin Panel</a>
+        
+    </div><!-- navbar-header finish -->
+    
+    <ul class="nav navbar-right top-nav"><!-- nav navbar-right top-nav begin -->
+        
+            
+            <a href="#" class="navbar-brand"><!-- dropdown-toggle begin -->
+                
+                <i class="fa fa-user" style="color: black;">&nbsp&nbsp<?php echo $admin_name; ?> </i>&nbsp&nbsp
+                
+            </a><!-- dropdown-toggle finish -->
+             
+    </ul><!-- nav navbar-right top-nav finish -->
+    
+    <div class="collapse navbar-collapse navbar-ex1-collapse"><!-- collapse navbar-collapse navbar-ex1-collapse begin -->
+        <ul class="nav navbar-nav side-nav"><!-- nav navbar-nav side-nav begin -->
+            <li><!-- li begin -->
+                <a href="index.php?dashboard"><!-- a href begin -->
+                        
+                        <i class="fa fa-fw fa-dashboard"></i> Dashboard
+                        
+                </a><!-- a href finish -->
+                
+            </li><!-- li finish -->
+                                   
+            <li><!-- li begin -->
+                <a href="index.php?view_customers"><!-- a href begin -->
+                    <i class="fa fa-fw fa-users"></i> View Customers
+                </a><!-- a href finish -->
+            </li><!-- li finish -->
+            
+            <li><!-- li begin -->
+                <a href="index.php?view_orders"><!-- a href begin -->
+                    <i class="fa fa-fw fa-book"></i> View Orders
+                </a><!-- a href finish -->
+            </li><!-- li finish -->
+            
+            <li><!-- li begin -->
+                <a href="index.php?view_payments"><!-- a href begin -->
+                    <i class="fa fa-fw fa-dollar"></i> View Payments
+                </a><!-- a href finish -->
+            </li><!-- li finish -->
+
+            <li><!-- li begin -->
+                <a href="#" data-toggle="collapse" data-target="#products"><!-- a href begin -->
+                        
+                        <i class="fa fa-fw fa-cubes"></i> Products
+                        <i class="fa fa-fw fa-caret-down"></i>
+                        
+                </a><!-- a href finish -->
+                
+                <ul id="products" class="collapse"><!-- collapse begin -->
+                    <li><!-- li begin -->
+                        <a href="index.php?insert_product"> Insert Product </a>
+                    </li><!-- li finish -->
+                    <li><!-- li begin -->
+                        <a href="index.php?view_products"> View Products </a>
+                    </li><!-- li finish -->
+                </ul><!-- collapse finish -->
+                
+            </li><!-- li finish -->
+            
+            
+            <li><!-- li begin -->
+                <a href="#" data-toggle="collapse" data-target="#users"><!-- a href begin -->
+                        
+                        <i class="fa fa-fw fa-cogs"></i> Admins
+                        <i class="fa fa-fw fa-caret-down"></i>
+                        
+                </a><!-- a href finish -->
+                
+                <ul id="users" class="collapse"><!-- collapse begin -->
+                    <li><!-- li begin -->
+                        <a href="index.php?insert_user"> Insert Admin </a>
+                    </li><!-- li finish -->
+                    <li><!-- li begin -->
+                        <a href="index.php?view_users"> View Admins </a>
+                    </li><!-- li finish -->
+                </ul><!-- collapse finish -->
+                
+            </li><!-- li finish -->
+            
+            <li><!-- li begin -->
+                <a href="logout.php"><!-- a href begin -->
+                    <i class="fa fa-fw fa-sign-out"></i> Log Out
+                </a><!-- a href finish -->
+            </li><!-- li finish -->
+            
+        </ul><!-- nav navbar-nav side-nav finish -->
+    </div><!-- collapse navbar-collapse navbar-ex1-collapse finish -->
+    
+</nav><!-- navbar navbar-inverse navbar-fixed-top finish -->
